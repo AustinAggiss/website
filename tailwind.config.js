6563229ab9e722f1c62632cf8ea4523a980a86ae
austin <austin@aggiss.dev>
2023-11-12 06:22:44 +0000

    Added config files for development across devices

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    fontFamily: {
+      'sans': ['Mona Sans', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
